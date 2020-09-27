d4c4c3b69664f950b1bb43892b5893de5c3e0fbd
Hannah <hannahjoo24@gmail.com>
2020-09-27 09:28:48 -0700

    Update min_heap and heap_sort

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,34 @@
-
+require_relative "min_heap"
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
+# Time Complexity:  O(n log n) => because we iterate over the list O(n) and `add` method take O(log n)
+# Space Complexity: O(n) => because we create a heap
+
+
 def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+  return list if list.length <= 1
+
+  heap = create_heap(list)
+  
+  i = 0
+  while i < list.length 
+    removed = heap.remove() # remove root
+    list[i] = removed
+    i += 1
+  end
+
+  return list
 end
+
+def create_heap(list)
+  heap = MinHeap.new()
+
+  list.each do |elem| 
+    heap.add(elem) 
+  end
+
+  return heap
+end
+
+
+# heap sort concept - https://www.interviewcake.com/concept/java/heapsort
--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -7,6 +7,9 @@ class HeapNode
   end
 end
 
+# concept - reference: https://www.interviewcake.com/concept/java/heap
+# https://learn-2.galvanize.com/cohorts/2173/blocks/867/content_files/06-heaps/01-heaps.md
+
 class MinHeap
 
   def initialize
@@ -14,18 +17,29 @@ class MinHeap
   end
 
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n) => because adding a value to the end of the array is O(1) + calling the heap_up method will take O(log n) since heap is built on a complete binary tree O(log n)
+  # Space Complexity: O(log n) => because of the system stack from the heap_up method
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    node = HeapNode.new(key, value)
+    @store << node
+
+    new_idx = @store.length - 1
+    heap_up(new_idx) if @store.length > 1 # heap up only if there is something to sort
   end
 
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n) => because heap is built on a complete binary tree 
+  # Space Complexity: O(log n)
+
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+    return if @store.empty? # do nothing
+
+    swap(0, -1) # swap the first and the last (Take the bottom level's right-most item and move it to the top, to fill in the hole.)
+    removed = @store.pop
+
+    heap_down() if @store.length > 1 # only if there is something to heap down
+    return removed.value
   end
 
 
@@ -44,10 +58,10 @@ class MinHeap
   end
 
   # This method returns true if the heap is empty
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(1)
+  # Space complexity: O(1)
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    return @store.length == 0
   end
 
   private
@@ -55,17 +69,42 @@ class MinHeap
   # This helper method takes an index and
   #  moves it up the heap, if it is less than it's parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(log n) => because heap is built on a complete binary tree
+  # Space complexity: O(log n) => because of the system stack
+  # recursive way
   def heap_up(index)
-    
+    return if index == 0 # do nothing
+
+    parent_idx = (index - 1) / 2  
+
+    if @store[index].key < @store[parent_idx].key
+      swap(index, parent_idx)
+      heap_up(parent_idx)
+    end
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
-  def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+  # recursive way
+  def heap_down(parent_index=0)
+    return if @store.empty?
+    
+    left_idx = (2 * parent_index) + 1
+    right_idx = (2 * parent_index) + 2
+    
+    return if @store[left_idx].nil?
+
+    if @store[right_idx]
+      @store[left_idx].key < @store[right_idx].key ? heap_down_helper(left_idx, parent_index) : heap_down_helper(right_idx, parent_index)
+    elsif (@store[right_idx].nil? && @store[left_idx].key < @store[parent_index].key) 
+      heap_down_helper(left_idx, parent_index) 
+    end
+  end
+
+  def heap_down_helper(child_idx, parent_idx)
+    swap(child_idx, parent_idx)
+    heap_down(child_idx)
   end
 
   # If you want a swap method... you're welcome
